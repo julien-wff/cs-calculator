f766c237bbf82851eb2b64868f4afee5f7e0cfe5
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-11-17 22:03:35 +0100

    feat: test for negative and decimal numbers

--- Calculator.Tests/commands/AddCommandTests.cs
+++ Calculator.Tests/commands/AddCommandTests.cs
@@ -65,12 +65,43 @@ public class AddCommandTests
     {
         // Arrange
         var addCommand = new AddCommand();
-        var args = new[] { "add", "2", "invalid" };
+        var args1 = new[] { "add", "invalid", "2" };
+        var args2 = new[] { "add", "2", "invalid" };
+
+        // Act
+        var result1 = addCommand.Run(args1);
+        var result2 = addCommand.Run(args2);
+
+        // Assert
+        Assert.False(result1);
+        Assert.False(result2);
+    }
+
+    [Fact]
+    public void Run_ShouldReturnTrue_SupportsNegativeNumbers()
+    {
+        // Arrange
+        var addCommand = new AddCommand();
+        var args = new[] { "add", "-2", "-3" };
 
         // Act
         var result = addCommand.Run(args);
 
         // Assert
-        Assert.False(result);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Run_ShouldReturnTrue_SupportsDecimals()
+    {
+        // Arrange
+        var addCommand = new AddCommand();
+        var args = new[] { "add", "2.5", "3.5" };
+
+        // Act
+        var result = addCommand.Run(args);
+
+        // Assert
+        Assert.True(result);
     }
 }
